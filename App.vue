e2093a5d1d19ce8ab95cac1960a14ac5ca80ca64
Oppani <Oppanium@googlemail.com>
2021-10-13 11:51:12 +0200

    inital app commit

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">Zuko App</text>
+  </view>
+</template>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
