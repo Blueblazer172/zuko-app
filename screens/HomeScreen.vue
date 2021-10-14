7c804c481119b6ed75e0874e2fb6b2fc344e00eb
Oppani <Oppanium@googlemail.com>
2021-10-14 10:56:27 +0200

    added vue-native-router, added screens-folder,added testpages

---
+++ screens/HomeScreen.vue
@@ -0,0 +1,34 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">redirect to QrCode</text>
+    <button title="go to QrCode!" @press="goToQrCode" />
+  </view>
+</template>
+
+<script>
+export default {
+ props: {
+    navigation: {
+      type: Object
+    }
+  },
+  methods: {
+    goToQrCode() {
+      this.navigation.navigate("QrCode")
+    }
+  },
+};
+</script>
+
+<style>
+.container {
+  flex: 1;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+}
+.text-color-primary {
+  color: blue;
+  font-size: 30;
+}
+</style>
