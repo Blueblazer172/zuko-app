32354b8474538eed45315c6b729a33659da12dda
has <hannes.stefani@outlook.de>
2021-10-20 16:45:03 +0200

    fixed spacing

--- App.vue
+++ App.vue
@@ -1,12 +1,12 @@
 <template>
-  <app-navigator></app-navigator>
+    <app-navigator></app-navigator>
 </template>
 
 <script>
-import { createAppContainer, createStackNavigator } from "vue-native-router";
-import { enableScreens } from "react-native-screens";
+import {createAppContainer, createStackNavigator} from "vue-native-router";
+import {enableScreens} from "react-native-screens";
 import Vue from "vue-native-core";
-import { VueNativeBase } from "native-base";
+import {VueNativeBase} from "native-base";
 import * as Font from "expo-font";
 import HomeScreen from "./screens/HomeScreen.vue";
 import QrCodeScreen from "./screens/QrCodeScreen.vue";
@@ -16,36 +16,36 @@ enableScreens(false);
 Vue.use(VueNativeBase);
 
 const StackNavigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    QrCode: QrCodeScreen,
-    Login: LoginScreen,
-  },
-  {
-    initialRouteName: "Home",
-  }
+    {
+        Home: HomeScreen,
+        QrCode: QrCodeScreen,
+        Login: LoginScreen,
+    },
+    {
+        initialRouteName: "Home",
+    }
 );
 const AppNavigator = createAppContainer(StackNavigator);
 
 export default {
-  name: "App",
-  components: { AppNavigator, VueNativeBase },
-  created() {
-    this.loadFonts();
-  },
-  methods: {
-    async loadFonts() {
-      try {
-        this.isAppReady = false;
-        await Font.loadAsync({
-          Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-        });
-        this.isAppReady = true;
-      } catch (error) {
-        console.log("some error occured", error);
-        this.isAppReady = true;
-      }
+    name: "App",
+    components: {AppNavigator, VueNativeBase},
+    created() {
+        this.loadFonts();
     },
-  },
+    methods: {
+        async loadFonts() {
+            try {
+                this.isAppReady = false;
+                await Font.loadAsync({
+                    Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+                });
+                this.isAppReady = true;
+            } catch (error) {
+                console.log("some error occured", error);
+                this.isAppReady = true;
+            }
+        },
+    },
 };
 </script>
