e0c9e2c4ec2661315e417a9d06dfffe2d7acfe16
has <hannes.stefani@outlook.de>
2021-10-20 16:24:43 +0200

    fixed deprecated module warnings

--- App.vue
+++ App.vue
@@ -4,12 +4,14 @@
 
 <script>
 import {createAppContainer, createStackNavigator} from "vue-native-router";
+import { enableScreens } from 'react-native-screens';
 import Vue from "vue-native-core";
 import {VueNativeBase} from "native-base";
 import HomeScreen from "./screens/HomeScreen.vue";
 import QrCodeScreen from "./screens/QrCodeScreen.vue";
 import LoginScreen from "./screens/Login.vue"
 
+enableScreens(false);
 Vue.use(VueNativeBase);
 
 const StackNavigator = createStackNavigator(
